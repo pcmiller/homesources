660c32a1a606385ba31fd70cc0f82a5feab5efbd
Phil Miller <pmiller@tripwire.com>
2013-09-06 12:59:26 -0700

    Added some key dependences for XML analysis in iTunes project.

---
+++ iTunesSpring/src/main/java/org/philco/iTunes/utils/UnexpectedElementTypeException.java
@@ -0,0 +1,9 @@
+package org.philco.iTunes.utils;
+
+public class UnexpectedElementTypeException extends Exception {
+	public UnexpectedElementTypeException(String qualifiedName,
+			ElementType[] types) {
+	}
+
+	private static final long serialVersionUID = 1L;
+}
